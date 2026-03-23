16564309898dc2abaede646035853fe3e8223ccd
Kurt Leenen <39231@ma-web.nl>
2026-03-23 12:01:01 +0100

    side bar sections

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -3,7 +3,21 @@ import styles from './sidenav.module.css';
 export function Sidenav() {
   return (
     <nav className={styles.sidenav}>
-      Sidenav Component
+      <div className={styles.sideSection} id="1">
+        1
+      </div>
+      <div className={styles.sideSection} id="2">
+        2
+      </div>
+      <div className={styles.sideSection} id="3">
+        3
+      </div>
+      <div className={styles.sideSection} id="4">
+        4
+      </div>
+      <div className={styles.sideSection} id="5">
+        5
+      </div>
     </nav>
   );
 };
