e31e2d5855d05a907f4bf5b871f5b68b082bc4ec
Kurt Leenen <39231@ma-web.nl>
2026-03-23 12:40:40 +0100

    routing and link

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,23 +1,45 @@
 import styles from './sidenav.module.css';
+import { BrowserRouter, Router, Link } from 'react-router-dom';
 
 export function Sidenav() {
   return (
     <nav className={styles.sidenav}>
-      <div className={styles.sideSection} id="1">
-        1
-      </div>
-      <div className={styles.sideSection} id="2">
-        2
-      </div>
-      <div className={styles.sideSection} id="3">
-        3
-      </div>
-      <div className={styles.sideSection} id="4">
-        4
-      </div>
-      <div className={styles.sideSection} id="5">
-        5
-      </div>
+      <BrowserRouter>
+        <div className={styles.sideSection} id="1">
+          <Link to="/">Home</Link>
+          <Link to="/Short">Shorts</Link>
+          <Link to="/Subbed">Subscriptions</Link>
+        </div>
+        <div className={styles.sideSection} id="2">
+          <Link to="/Feed/You">You {">"}</Link>
+          <Link to="/Feed/History">History</Link>
+          <Link to="/PlyLs/WthLtr">Watch Later</Link>
+          <Link to="/PlyLs/LkdVds">Liked Videos</Link>
+          <Link to="/Feed/Dlds">Downloads</Link>
+        </div>
+        <div className={styles.sideSection} id="3">
+          <h3>Explore</h3>
+          <Link to="/">Music</Link>
+          <Link to="/Short">Movies</Link>
+          <Link to="/Subbed">Gaming</Link>
+          <Link to="/Nws">News</Link>
+          <Link to="/Spt">Sport</Link>
+          <Link to="/Pdcst">Podcast</Link>
+        </div>
+        <div className={styles.sideSection} id="4">
+          <h3>
+            More from OurTube
+          </h3>
+          <Link to="/">Home</Link>
+          <Link to="/Short">Shorts</Link>
+          <Link to="/Subbed">Subscriptions</Link>
+        </div>
+        <div className={styles.sideSection} id="5">
+          <Link to="/">Home</Link>
+          <Link to="/Short">Shorts</Link>
+          <Link to="/Subbed">Subscriptions</Link>
+        </div>
+      </BrowserRouter>
     </nav>
   );
 };
